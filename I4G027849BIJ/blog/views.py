4e5d5366083b7cef4d5b69e0177b8ec7cacd5a2c
christian <christianajulor@gmail.com>
2022-06-21 14:51:18 +0100

    initial commit

---
+++ I4G027849BIJ/blog/views.py
@@ -0,0 +1,32 @@
+from msilib.schema import Class
+from django.shortcuts import render
+from .models import Post 
+from re import template
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
